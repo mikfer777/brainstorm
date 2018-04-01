7b14f51f65186d15708d2b816ee745180015f7d4
Michel Ferreol <michel.ferreol@atos.net>
2018-04-01 15:31:10 +0200

    reset venv

--- pytest/venv/Scripts/activate.bat
+++ pytest/venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\rootWorknew\pytest\venv"
+set "VIRTUAL_ENV=C:\rootGit\Github\brainstorm\pytest\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
